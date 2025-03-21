24fa60c6b535d653abad77a868a2b48e12c8a11f
Eric <mateieric66@gmail.com>
2025-03-21 22:46:35 +0300

    Update eric.js

--- eric.js
+++ eric.js
@@ -1,3 +1,6 @@
+<script src="https://cdn.logwork.com/widget/countdown.js"></script>
+<a href="https://logwork.com/countdown-timer" class="countdown-timer" data-timezone="Africa/Nairobi" data-date="2025-03-23 22:43">Countdown Timer</a>
+    
 function toggleMenu() {
     var navbar = document.getElementById("navbar");
     if (navbar.classList.contains("show")) {
